eb526e58fe537148c17e72a239ac7283134f8c4b
Henrique Martins <72051170+Henrique0501@users.noreply.github.com>
2022-06-01 12:05:38 -0300

    Delete config.py

--- app/config.py
+++
@@ -1,7 +0,0 @@
-from datetime import timedelta
-
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:#Henrique0501#@localhost/api_imoveis'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "My secret key that nobody knows"
-JWT_SECRET_KEY = "If I tell you I will have to kill you"
-JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
